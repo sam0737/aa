78c83992844efd3934bbb2f5123eb16bb5a27f87
Sam Wong <sam@hellosam.net>
2016-09-12 02:05:51 +0800

    v0.2.0

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {BookService, BookAbstract, Book} from './../../lib/aabook';
 import {TranslatePipe} from "ng2-translate/ng2-translate";
 
-let VERSION = '0.1';
+let VERSION = '0.2.0';
 
 @Component({
   templateUrl: 'build/pages/about/about.html',
